63a086f50995ab1ed744c834bad4c7185606a8b1
noelkawm <60691371+noelkawm@users.noreply.github.com>
2024-07-02 12:02:39 +0100

    Update Creating mask of a ball.m

--- Creating mask of a ball.m
+++ Creating mask of a ball.m
@@ -3,7 +3,7 @@ close all;
 clc;
 
 videoObj = VideoReader("nogoal.mp4");% VideoReader to read frames of video
-newVideo = VideoWriter("C:/Users/theno/Downloads/Image processing/taskDvideoNoGoal", 'MPEG-4');
+newVideo = VideoWriter("taskDvideoNoGoal", 'MPEG-4');
 %VideoWriter for saving created frames of a video
 open(newVideo);%Openning VideoWriter object for wrtiting
 
